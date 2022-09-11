8db9fa1e037e417fe23117613ffbb5165c83b142
Santiago <gamersanti19@gmail.com>
2022-09-11 10:04:40 -0500

    Initial project

---
+++ src/test/java/com/acs/categorias/api/ApiApplicationTests.java
@@ -0,0 +1,13 @@
+package com.acs.categorias.api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
